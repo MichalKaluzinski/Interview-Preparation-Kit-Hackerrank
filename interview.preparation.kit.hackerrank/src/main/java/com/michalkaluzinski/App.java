f12319b41eb85fb2ff3c03e4ec9af9f51e844809
Michal Kaluzinski <m.kaluzinski@devesoft.pl>
2019-10-24 10:24:14 +0200

    initialzie project

---
+++ interview.preparation.kit.hackerrank/src/main/java/com/michalkaluzinski/App.java
@@ -0,0 +1,13 @@
+package com.michalkaluzinski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
